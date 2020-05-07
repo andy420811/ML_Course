4022ac33716dbe64700b831095fbbbb1950e734e
andy420811 <62095826+andy420811@users.noreply.github.com>
2020-05-07 18:31:14 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -7,7 +7,6 @@ from mlgame.communication import ml as comm
 import numpy as np
 import pickle
 from os import path
-from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
